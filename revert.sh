f00bf265162e0d63a048ba899546d8b1eb280fe1
Leke Ariyo <37749047+Leke-Ariyo@users.noreply.github.com>
2022-01-21 18:52:13 +0100

    Update revert.sh

--- revert.sh
+++ revert.sh
@@ -17,5 +17,6 @@ git config --global user.name "GitHub Revert Action"
 set -o xtrace
 
 git fetch origin main
-git reset --hard HEAD~1
+git revert HEAD~1
+#git reset --hard HEAD~1
 git push origin main
